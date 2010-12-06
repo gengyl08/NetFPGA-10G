02d2e7a646c1d551180fd450b25844c9ee9d38f8
Michaela Blott <mblott@xirlsgp1.(none)>
2010-12-06 14:43:41 +0000

    generated and checked packet against a format stored in ROM; programmable IFG

--- lib/hw/std/pcores/nf10_axis_gen_check_v1_00_a/hdl/vhdl/nf10_axis_gen_check.vhd
+++ lib/hw/std/pcores/nf10_axis_gen_check_v1_00_a/hdl/vhdl/nf10_axis_gen_check.vhd
@@ -4,6 +4,7 @@
 -- 
 -- Description:
 -- Hardware component that generates and checks packets
+-- 64bit only when tx,rx of hardcoded packet
 --
 -- May 2010
 ---------------------------------------------------------------------------------
@@ -15,8 +16,10 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity nf10_axis_gen_check is
 generic (
-   C_M_AXIS_DATA_WIDTH : integer := 64;
-   C_S_AXIS_DATA_WIDTH : integer := 64   
+   C_M_AXIS_DATA_WIDTH : integer := 64; -- max 64bit supported
+   C_S_AXIS_DATA_WIDTH : integer := 64; -- max 64bit supported
+   PKT_SIZE            : integer := 10; -- in 64bit words; pkt_size + ifg_size < 65k=number of provisioned states  
+   IFG_SIZE            : integer := 50 -- in 64bit words irrespective of backpressure
 );
 port (
    ACLK               : in  std_logic;
@@ -36,6 +39,20 @@ end nf10_axis_gen_check;
 
 architecture structural of nf10_axis_gen_check is
 
+   type rom_type is array (0 to PKT_SIZE-1) of std_logic_vector (C_S_AXIS_DATA_WIDTH-1 downto 0);
+   constant gen_ROM, check_ROM : rom_type :=
+	                          (x"0002cfbddde50005",x"9a3c780008004500",
+                              x"0040874c40008006",x"450dac10e80ad02b",
+                              x"ca1705d90050c5e3",x"75ce00000000b002",
+                              x"ffffc68d00000204",x"04ec010303020101",
+                              x"080a000000000000",x"0000010104020000");     
+   --0002cfbddde50005 9a3c780008004500
+   --0040874c40008006 450dac10e80ad02b
+   --ca1705d90050c5e3 75ce00000000b002
+   --ffffc68d00000204 04ec010303020101
+   --080a000000000000 000001010402 
+   -- ROM should be inferred as BRAM during XST
+	
    signal gen_state          : std_logic_vector(15 downto 0);
    signal check_state        : std_logic_vector(15 downto 0);
    signal tx_count           : std_logic_vector(31 downto 0);
@@ -43,37 +60,42 @@ architecture structural of nf10_axis_gen_check is
    signal err_count          : std_logic_vector(31 downto 0);
    signal ok                 : std_logic;
    signal last_data_plus_one : std_logic_vector(3 downto 0);	
-   
+   signal gen_rom_addr       : std_logic_vector(15 downto 0);
+   signal check_rom_addr     : std_logic_vector(15 downto 0);
+	signal pkt_data_buf       : std_logic_vector(C_S_AXIS_DATA_WIDTH-1 downto 0);
+	
 begin
+
 
 gen_p: process(ACLK)
 begin
    if (ARESETN='0') then
       gen_state <= (others => '0');
       tx_count <= (others => '0');
+      gen_rom_addr <= (others => '0');
    elsif (ACLK = '1' and ACLK'event) then
-      case gen_state is
-      when x"0000" =>
+      gen_rom_addr <= gen_state; -- addr needs to be gen_state -1- so always 1 tick behind  
+      if gen_state = x"0000" then
          M_AXIS_TDATA <= (others => '0');
-			M_AXIS_TSTRB <= (others => '0');
-			M_AXIS_TVALID <= '0';
-			M_AXIS_TLAST <= '0';
+         M_AXIS_TSTRB <= (others => '0');
+         M_AXIS_TVALID <= '0';
+         M_AXIS_TLAST <= '0';
          if (M_AXIS_TREADY='1') then
             gen_state <= gen_state + 1;
          end if;
-      when x"0001"|x"0002"|x"0003"|x"0004"|x"0005"|x"0006"|x"0007"|x"0008" =>
-         for i in 0 to C_M_AXIS_DATA_WIDTH/8 -1 loop
-            M_AXIS_TDATA(i+7 downto i) <= gen_state(3 downto 0) & gen_state(3 downto 0);
+      elsif gen_state < PKT_SIZE then 
+         for i in 0 to C_M_AXIS_DATA_WIDTH-1 loop
+			   M_AXIS_TDATA(i) <= gen_ROM(conv_integer(gen_rom_addr))(i);
          end loop;
-         M_AXIS_TSTRB <= (others => '1');
+			M_AXIS_TSTRB <= (others => '1');
          M_AXIS_TVALID <= '1';
          M_AXIS_TLAST <= '0';
          if (M_AXIS_TREADY='1') then
             gen_state <= gen_state + 1;
          end if;	
-      when x"0009" =>
-         for i in 0 to C_M_AXIS_DATA_WIDTH/8 -1 loop
-            M_AXIS_TDATA(i+7 downto i) <= gen_state(3 downto 0) & gen_state(3 downto 0);
+      elsif gen_state = PKT_SIZE then
+         for i in 0 to C_M_AXIS_DATA_WIDTH-1 loop
+			   M_AXIS_TDATA(i) <= gen_ROM(conv_integer(gen_rom_addr))(i);
          end loop;
          M_AXIS_TSTRB <= (others => '1');
          M_AXIS_TVALID <= '1';
@@ -82,19 +104,19 @@ begin
             tx_count <= tx_count + 1;	
             gen_state <= gen_state + 1;
          end if;	
-      when x"000F" =>
+      elsif gen_state < PKT_SIZE+IFG_SIZE-1 then
          M_AXIS_TDATA <= (others => '0');
          M_AXIS_TSTRB <= (others => '0');
          M_AXIS_TVALID <= '0';
          M_AXIS_TLAST <= '0';
-         gen_state <= (others => '0');		      		
-      when others =>
-         M_AXIS_TDATA <= x"0000000000000000";
-         M_AXIS_TSTRB <= x"00";
-         M_AXIS_TVALID <= '0';
+         gen_state <= gen_state + 1;		      		
+      else
+         M_AXIS_TDATA <= (others => '0');
+         M_AXIS_TSTRB <= (others => '0');
+         M_AXIS_TVALID <= '0';			
          M_AXIS_TLAST <= '0';
-         gen_state <= gen_state + 1;	
-      end case;	
+         gen_state <= (others => '0');	
+      end if;	
    end if;
 end process;
 
@@ -104,35 +126,41 @@ begin
    if (ARESETN='0') then
       check_state <= (others => '0');
       rx_count <= (others => '0');
+		err_count <= (others => '0');
       ok <= '1';
+		check_rom_addr <= x"0000"; 
+		pkt_data_buf <= (others => '0');
    elsif (ACLK = '1' and ACLK'event) then
-      case check_state is
-      when x"0000" =>
+	   pkt_data_buf <= S_AXIS_TDATA;
+      if check_state = x"0000" then
+         -- waiting for a pkt
          if S_AXIS_TVALID = '1' then
-            last_data_plus_one <= x"1";
             ok <= '1';
+            check_rom_addr <= x"0000"; 
             check_state <= check_state + 1;
          end if;
-      when x"0001" =>
-         for i in 0 to C_M_AXIS_DATA_WIDTH/8 -1 loop
-            if S_AXIS_TDATA(i+7 downto i) = last_data_plus_one & last_data_plus_one then
+      elsif check_state = x"0001" then
+		   -- checking the packet against ROM
+         for i in 0 to C_M_AXIS_DATA_WIDTH-1 loop
+            if pkt_data_buf(i) = check_ROM(conv_integer(check_rom_addr))(i) then
                ok <= ok;
             else
                ok <= '0';
             end if; 
-         end loop;         
+		   end loop;
+			check_rom_addr <= check_rom_addr + 1;			
          if S_AXIS_TVALID = '1' and S_AXIS_TLAST = '1' then
             check_state <= check_state + 1;
-            last_data_plus_one <= last_data_plus_one + 1;
           end if;		    
-      when x"0002" =>
-         rx_count <= rx_count + 1;
+      elsif check_state = x"0002" then
+         if (ok='1') then
+			   rx_count <= rx_count + 1;
+			else
+			   err_count <= err_count + 1;
+			end if;
          check_state <= (others => '0'); 			
-      when others =>
-      end case;
+      end if;
    end if;
 end process;
-
-
 
 end structural;
