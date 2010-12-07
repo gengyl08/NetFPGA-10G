377dbd180545570b56b0aac2a0f65a28b1fbea02
eastzone <hyzeng@stanford.edu>
2010-12-07 10:26:39 -0800

    simulation: Added a simple simulation environment

---
+++ projects/loopback_test/hw/nf10/system_tb.v
@@ -0,0 +1,276 @@
+//-----------------------------------------------------------------------------
+// nf10_tb.v
+//-----------------------------------------------------------------------------
+
+`timescale 1 ns / 1ps
+
+`uselib lib=unisims_ver
+
+// START USER CODE (Do not remove this line)
+
+// User: Put your directives here. Code in this
+//       section will not be overwritten.
+
+// END USER CODE (Do not remove this line)
+
+module system_tb
+  (
+  );
+
+  // START USER CODE (Do not remove this line)
+
+  // User: Put your signals here. Code in this
+  //       section will not be overwritten.
+  integer             i;
+  
+  // END USER CODE (Do not remove this line)
+
+  reg RESET;
+  wire RS232_Uart_1_sout;
+  reg RS232_Uart_1_sin;
+  reg CLK;
+  wire nf10_10g_interface_0_xaui_tx_l0_p_pin;
+  wire nf10_10g_interface_0_xaui_tx_l0_n_pin;
+  wire nf10_10g_interface_0_xaui_tx_l1_p_pin;
+  wire nf10_10g_interface_0_xaui_tx_l1_n_pin;
+  wire nf10_10g_interface_0_xaui_tx_l2_p_pin;
+  wire nf10_10g_interface_0_xaui_tx_l3_n_pin;
+  wire nf10_10g_interface_0_xaui_tx_l2_n_pin;
+  wire nf10_10g_interface_0_xaui_tx_l3_p_pin;
+  reg nf10_10g_interface_0_xaui_rx_l0_n_pin;
+  reg nf10_10g_interface_0_xaui_rx_l1_p_pin;
+  reg nf10_10g_interface_0_xaui_rx_l1_n_pin;
+  reg nf10_10g_interface_0_xaui_rx_l2_p_pin;
+  reg nf10_10g_interface_0_xaui_rx_l2_n_pin;
+  reg nf10_10g_interface_0_xaui_rx_l3_p_pin;
+  reg nf10_10g_interface_0_xaui_rx_l3_n_pin;
+  reg nf10_10g_interface_0_xaui_rx_l0_p_pin;
+  wire nf10_10g_interface_1_xaui_tx_l3_n_pin;
+  wire nf10_10g_interface_1_xaui_tx_l1_n_pin;
+  wire nf10_10g_interface_1_xaui_tx_l3_p_pin;
+  wire nf10_10g_interface_1_xaui_tx_l0_p_pin;
+  wire nf10_10g_interface_1_xaui_tx_l0_n_pin;
+  wire nf10_10g_interface_1_xaui_tx_l1_p_pin;
+  wire nf10_10g_interface_1_xaui_tx_l2_n_pin;
+  reg nf10_10g_interface_1_xaui_rx_l0_p_pin;
+  reg nf10_10g_interface_1_xaui_rx_l0_n_pin;
+  reg nf10_10g_interface_1_xaui_rx_l1_p_pin;
+  reg nf10_10g_interface_1_xaui_rx_l1_n_pin;
+  reg nf10_10g_interface_1_xaui_rx_l2_p_pin;
+  reg nf10_10g_interface_1_xaui_rx_l2_n_pin;
+  reg nf10_10g_interface_1_xaui_rx_l3_p_pin;
+  reg nf10_10g_interface_1_xaui_rx_l3_n_pin;
+  wire nf10_10g_interface_1_xaui_tx_l2_p_pin;
+  wire nf10_10g_interface_2_xaui_tx_l3_p_pin;
+  wire nf10_10g_interface_2_xaui_tx_l3_n_pin;
+  wire nf10_10g_interface_2_xaui_tx_l0_p_pin;
+  wire nf10_10g_interface_2_xaui_tx_l0_n_pin;
+  wire nf10_10g_interface_2_xaui_tx_l1_p_pin;
+  wire nf10_10g_interface_2_xaui_tx_l2_p_pin;
+  wire nf10_10g_interface_2_xaui_tx_l2_n_pin;
+  reg nf10_10g_interface_2_xaui_rx_l0_p_pin;
+  reg nf10_10g_interface_2_xaui_rx_l0_n_pin;
+  reg nf10_10g_interface_2_xaui_rx_l1_p_pin;
+  reg nf10_10g_interface_2_xaui_rx_l1_n_pin;
+  reg nf10_10g_interface_2_xaui_rx_l2_p_pin;
+  reg nf10_10g_interface_2_xaui_rx_l2_n_pin;
+  reg nf10_10g_interface_2_xaui_rx_l3_p_pin;
+  reg nf10_10g_interface_2_xaui_rx_l3_n_pin;
+  wire nf10_10g_interface_2_xaui_tx_l1_n_pin;
+  wire nf10_10g_interface_3_xaui_tx_l3_p_pin;
+  wire nf10_10g_interface_3_xaui_tx_l3_n_pin;
+  wire nf10_10g_interface_3_xaui_tx_l0_p_pin;
+  wire nf10_10g_interface_3_xaui_tx_l0_n_pin;
+  wire nf10_10g_interface_3_xaui_tx_l1_p_pin;
+  wire nf10_10g_interface_3_xaui_tx_l2_p_pin;
+  wire nf10_10g_interface_3_xaui_tx_l2_n_pin;
+  reg nf10_10g_interface_3_xaui_rx_l0_p_pin;
+  reg nf10_10g_interface_3_xaui_rx_l0_n_pin;
+  reg nf10_10g_interface_3_xaui_rx_l1_p_pin;
+  reg nf10_10g_interface_3_xaui_rx_l1_n_pin;
+  reg nf10_10g_interface_3_xaui_rx_l2_p_pin;
+  reg nf10_10g_interface_3_xaui_rx_l2_n_pin;
+  reg nf10_10g_interface_3_xaui_rx_l3_p_pin;
+  reg nf10_10g_interface_3_xaui_rx_l3_n_pin;
+  wire nf10_10g_interface_3_xaui_tx_l1_n_pin;
+  reg refclk_A_p;
+  reg refclk_A_n;
+  reg refclk_B_p;
+  reg refclk_B_n;
+  reg refclk_C_p;
+  reg refclk_C_n;
+  reg refclk_D_p;
+  reg refclk_D_n;
+  wire MDC;
+  wire MDIO;
+  wire PHY_RST_N;
+
+  system
+    dut (
+      .RESET ( RESET ),
+      .RS232_Uart_1_sout ( RS232_Uart_1_sout ),
+      .RS232_Uart_1_sin ( RS232_Uart_1_sin ),
+      .CLK ( CLK ),
+      .nf10_10g_interface_0_xaui_tx_l0_p_pin ( nf10_10g_interface_0_xaui_tx_l0_p_pin ),
+      .nf10_10g_interface_0_xaui_tx_l0_n_pin ( nf10_10g_interface_0_xaui_tx_l0_n_pin ),
+      .nf10_10g_interface_0_xaui_tx_l1_p_pin ( nf10_10g_interface_0_xaui_tx_l1_p_pin ),
+      .nf10_10g_interface_0_xaui_tx_l1_n_pin ( nf10_10g_interface_0_xaui_tx_l1_n_pin ),
+      .nf10_10g_interface_0_xaui_tx_l2_p_pin ( nf10_10g_interface_0_xaui_tx_l2_p_pin ),
+      .nf10_10g_interface_0_xaui_tx_l3_n_pin ( nf10_10g_interface_0_xaui_tx_l3_n_pin ),
+      .nf10_10g_interface_0_xaui_tx_l2_n_pin ( nf10_10g_interface_0_xaui_tx_l2_n_pin ),
+      .nf10_10g_interface_0_xaui_tx_l3_p_pin ( nf10_10g_interface_0_xaui_tx_l3_p_pin ),
+      .nf10_10g_interface_0_xaui_rx_l0_n_pin ( nf10_10g_interface_0_xaui_rx_l0_n_pin ),
+      .nf10_10g_interface_0_xaui_rx_l1_p_pin ( nf10_10g_interface_0_xaui_rx_l1_p_pin ),
+      .nf10_10g_interface_0_xaui_rx_l1_n_pin ( nf10_10g_interface_0_xaui_rx_l1_n_pin ),
+      .nf10_10g_interface_0_xaui_rx_l2_p_pin ( nf10_10g_interface_0_xaui_rx_l2_p_pin ),
+      .nf10_10g_interface_0_xaui_rx_l2_n_pin ( nf10_10g_interface_0_xaui_rx_l2_n_pin ),
+      .nf10_10g_interface_0_xaui_rx_l3_p_pin ( nf10_10g_interface_0_xaui_rx_l3_p_pin ),
+      .nf10_10g_interface_0_xaui_rx_l3_n_pin ( nf10_10g_interface_0_xaui_rx_l3_n_pin ),
+      .nf10_10g_interface_0_xaui_rx_l0_p_pin ( nf10_10g_interface_0_xaui_rx_l0_p_pin ),
+      .nf10_10g_interface_1_xaui_tx_l3_n_pin ( nf10_10g_interface_1_xaui_tx_l3_n_pin ),
+      .nf10_10g_interface_1_xaui_tx_l1_n_pin ( nf10_10g_interface_1_xaui_tx_l1_n_pin ),
+      .nf10_10g_interface_1_xaui_tx_l3_p_pin ( nf10_10g_interface_1_xaui_tx_l3_p_pin ),
+      .nf10_10g_interface_1_xaui_tx_l0_p_pin ( nf10_10g_interface_1_xaui_tx_l0_p_pin ),
+      .nf10_10g_interface_1_xaui_tx_l0_n_pin ( nf10_10g_interface_1_xaui_tx_l0_n_pin ),
+      .nf10_10g_interface_1_xaui_tx_l1_p_pin ( nf10_10g_interface_1_xaui_tx_l1_p_pin ),
+      .nf10_10g_interface_1_xaui_tx_l2_n_pin ( nf10_10g_interface_1_xaui_tx_l2_n_pin ),
+      .nf10_10g_interface_1_xaui_rx_l0_p_pin ( nf10_10g_interface_1_xaui_rx_l0_p_pin ),
+      .nf10_10g_interface_1_xaui_rx_l0_n_pin ( nf10_10g_interface_1_xaui_rx_l0_n_pin ),
+      .nf10_10g_interface_1_xaui_rx_l1_p_pin ( nf10_10g_interface_1_xaui_rx_l1_p_pin ),
+      .nf10_10g_interface_1_xaui_rx_l1_n_pin ( nf10_10g_interface_1_xaui_rx_l1_n_pin ),
+      .nf10_10g_interface_1_xaui_rx_l2_p_pin ( nf10_10g_interface_1_xaui_rx_l2_p_pin ),
+      .nf10_10g_interface_1_xaui_rx_l2_n_pin ( nf10_10g_interface_1_xaui_rx_l2_n_pin ),
+      .nf10_10g_interface_1_xaui_rx_l3_p_pin ( nf10_10g_interface_1_xaui_rx_l3_p_pin ),
+      .nf10_10g_interface_1_xaui_rx_l3_n_pin ( nf10_10g_interface_1_xaui_rx_l3_n_pin ),
+      .nf10_10g_interface_1_xaui_tx_l2_p_pin ( nf10_10g_interface_1_xaui_tx_l2_p_pin ),
+      .nf10_10g_interface_2_xaui_tx_l3_p_pin ( nf10_10g_interface_2_xaui_tx_l3_p_pin ),
+      .nf10_10g_interface_2_xaui_tx_l3_n_pin ( nf10_10g_interface_2_xaui_tx_l3_n_pin ),
+      .nf10_10g_interface_2_xaui_tx_l0_p_pin ( nf10_10g_interface_2_xaui_tx_l0_p_pin ),
+      .nf10_10g_interface_2_xaui_tx_l0_n_pin ( nf10_10g_interface_2_xaui_tx_l0_n_pin ),
+      .nf10_10g_interface_2_xaui_tx_l1_p_pin ( nf10_10g_interface_2_xaui_tx_l1_p_pin ),
+      .nf10_10g_interface_2_xaui_tx_l2_p_pin ( nf10_10g_interface_2_xaui_tx_l2_p_pin ),
+      .nf10_10g_interface_2_xaui_tx_l2_n_pin ( nf10_10g_interface_2_xaui_tx_l2_n_pin ),
+      .nf10_10g_interface_2_xaui_rx_l0_p_pin ( nf10_10g_interface_2_xaui_rx_l0_p_pin ),
+      .nf10_10g_interface_2_xaui_rx_l0_n_pin ( nf10_10g_interface_2_xaui_rx_l0_n_pin ),
+      .nf10_10g_interface_2_xaui_rx_l1_p_pin ( nf10_10g_interface_2_xaui_rx_l1_p_pin ),
+      .nf10_10g_interface_2_xaui_rx_l1_n_pin ( nf10_10g_interface_2_xaui_rx_l1_n_pin ),
+      .nf10_10g_interface_2_xaui_rx_l2_p_pin ( nf10_10g_interface_2_xaui_rx_l2_p_pin ),
+      .nf10_10g_interface_2_xaui_rx_l2_n_pin ( nf10_10g_interface_2_xaui_rx_l2_n_pin ),
+      .nf10_10g_interface_2_xaui_rx_l3_p_pin ( nf10_10g_interface_2_xaui_rx_l3_p_pin ),
+      .nf10_10g_interface_2_xaui_rx_l3_n_pin ( nf10_10g_interface_2_xaui_rx_l3_n_pin ),
+      .nf10_10g_interface_2_xaui_tx_l1_n_pin ( nf10_10g_interface_2_xaui_tx_l1_n_pin ),
+      .nf10_10g_interface_3_xaui_tx_l3_p_pin ( nf10_10g_interface_3_xaui_tx_l3_p_pin ),
+      .nf10_10g_interface_3_xaui_tx_l3_n_pin ( nf10_10g_interface_3_xaui_tx_l3_n_pin ),
+      .nf10_10g_interface_3_xaui_tx_l0_p_pin ( nf10_10g_interface_3_xaui_tx_l0_p_pin ),
+      .nf10_10g_interface_3_xaui_tx_l0_n_pin ( nf10_10g_interface_3_xaui_tx_l0_n_pin ),
+      .nf10_10g_interface_3_xaui_tx_l1_p_pin ( nf10_10g_interface_3_xaui_tx_l1_p_pin ),
+      .nf10_10g_interface_3_xaui_tx_l2_p_pin ( nf10_10g_interface_3_xaui_tx_l2_p_pin ),
+      .nf10_10g_interface_3_xaui_tx_l2_n_pin ( nf10_10g_interface_3_xaui_tx_l2_n_pin ),
+      .nf10_10g_interface_3_xaui_rx_l0_p_pin ( nf10_10g_interface_3_xaui_rx_l0_p_pin ),
+      .nf10_10g_interface_3_xaui_rx_l0_n_pin ( nf10_10g_interface_3_xaui_rx_l0_n_pin ),
+      .nf10_10g_interface_3_xaui_rx_l1_p_pin ( nf10_10g_interface_3_xaui_rx_l1_p_pin ),
+      .nf10_10g_interface_3_xaui_rx_l1_n_pin ( nf10_10g_interface_3_xaui_rx_l1_n_pin ),
+      .nf10_10g_interface_3_xaui_rx_l2_p_pin ( nf10_10g_interface_3_xaui_rx_l2_p_pin ),
+      .nf10_10g_interface_3_xaui_rx_l2_n_pin ( nf10_10g_interface_3_xaui_rx_l2_n_pin ),
+      .nf10_10g_interface_3_xaui_rx_l3_p_pin ( nf10_10g_interface_3_xaui_rx_l3_p_pin ),
+      .nf10_10g_interface_3_xaui_rx_l3_n_pin ( nf10_10g_interface_3_xaui_rx_l3_n_pin ),
+      .nf10_10g_interface_3_xaui_tx_l1_n_pin ( nf10_10g_interface_3_xaui_tx_l1_n_pin ),
+      .refclk_A_p ( refclk_A_p ),
+      .refclk_A_n ( refclk_A_n ),
+      .refclk_B_p ( refclk_B_p ),
+      .refclk_B_n ( refclk_B_n ),
+      .refclk_C_p ( refclk_C_p ),
+      .refclk_C_n ( refclk_C_n ),
+      .refclk_D_p ( refclk_D_p ),
+      .refclk_D_n ( refclk_D_n ),
+      .MDC ( MDC ),
+      .MDIO ( MDIO ),
+      .PHY_RST_N ( PHY_RST_N )
+    );
+
+
+  // START USER CODE (Do not remove this line)
+
+  // User: Put your stimulus here. Code in this
+  //       section will not be overwritten.
+  
+  // Part 1: Wire connection
+  always @(*) begin
+      // Port 0 to Port 1
+      nf10_10g_interface_0_xaui_rx_l0_p_pin = nf10_10g_interface_1_xaui_tx_l0_p_pin;
+      nf10_10g_interface_0_xaui_rx_l0_n_pin = nf10_10g_interface_1_xaui_tx_l0_n_pin;
+      nf10_10g_interface_0_xaui_rx_l1_p_pin = nf10_10g_interface_1_xaui_tx_l1_p_pin;
+      nf10_10g_interface_0_xaui_rx_l1_n_pin = nf10_10g_interface_1_xaui_tx_l1_n_pin;
+      nf10_10g_interface_0_xaui_rx_l2_p_pin = nf10_10g_interface_1_xaui_tx_l2_p_pin;
+      nf10_10g_interface_0_xaui_rx_l2_n_pin = nf10_10g_interface_1_xaui_tx_l2_n_pin;
+      nf10_10g_interface_0_xaui_rx_l3_p_pin = nf10_10g_interface_1_xaui_tx_l3_p_pin;
+      nf10_10g_interface_0_xaui_rx_l3_n_pin = nf10_10g_interface_1_xaui_tx_l3_n_pin;
+      
+      nf10_10g_interface_1_xaui_rx_l0_p_pin = nf10_10g_interface_0_xaui_tx_l0_p_pin;
+      nf10_10g_interface_1_xaui_rx_l0_n_pin = nf10_10g_interface_0_xaui_tx_l0_n_pin;
+      nf10_10g_interface_1_xaui_rx_l1_p_pin = nf10_10g_interface_0_xaui_tx_l1_p_pin;
+      nf10_10g_interface_1_xaui_rx_l1_n_pin = nf10_10g_interface_0_xaui_tx_l1_n_pin;
+      nf10_10g_interface_1_xaui_rx_l2_p_pin = nf10_10g_interface_0_xaui_tx_l2_p_pin;
+      nf10_10g_interface_1_xaui_rx_l2_n_pin = nf10_10g_interface_0_xaui_tx_l2_n_pin;
+      nf10_10g_interface_1_xaui_rx_l3_p_pin = nf10_10g_interface_0_xaui_tx_l3_p_pin;
+      nf10_10g_interface_1_xaui_rx_l3_n_pin = nf10_10g_interface_0_xaui_tx_l3_n_pin;
+      
+      // Port 2 to Port 3
+      nf10_10g_interface_2_xaui_rx_l0_p_pin = nf10_10g_interface_3_xaui_tx_l0_p_pin;
+      nf10_10g_interface_2_xaui_rx_l0_n_pin = nf10_10g_interface_3_xaui_tx_l0_n_pin;
+      nf10_10g_interface_2_xaui_rx_l1_p_pin = nf10_10g_interface_3_xaui_tx_l1_p_pin;
+      nf10_10g_interface_2_xaui_rx_l1_n_pin = nf10_10g_interface_3_xaui_tx_l1_n_pin;
+      nf10_10g_interface_2_xaui_rx_l2_p_pin = nf10_10g_interface_3_xaui_tx_l2_p_pin;
+      nf10_10g_interface_2_xaui_rx_l2_n_pin = nf10_10g_interface_3_xaui_tx_l2_n_pin;
+      nf10_10g_interface_2_xaui_rx_l3_p_pin = nf10_10g_interface_3_xaui_tx_l3_p_pin;
+      nf10_10g_interface_2_xaui_rx_l3_n_pin = nf10_10g_interface_3_xaui_tx_l3_n_pin;
+      
+      nf10_10g_interface_3_xaui_rx_l0_p_pin = nf10_10g_interface_2_xaui_tx_l0_p_pin;
+      nf10_10g_interface_3_xaui_rx_l0_n_pin = nf10_10g_interface_2_xaui_tx_l0_n_pin;
+      nf10_10g_interface_3_xaui_rx_l1_p_pin = nf10_10g_interface_2_xaui_tx_l1_p_pin;
+      nf10_10g_interface_3_xaui_rx_l1_n_pin = nf10_10g_interface_2_xaui_tx_l1_n_pin;
+      nf10_10g_interface_3_xaui_rx_l2_p_pin = nf10_10g_interface_2_xaui_tx_l2_p_pin;
+      nf10_10g_interface_3_xaui_rx_l2_n_pin = nf10_10g_interface_2_xaui_tx_l2_n_pin;
+      nf10_10g_interface_3_xaui_rx_l3_p_pin = nf10_10g_interface_2_xaui_tx_l3_p_pin;
+      nf10_10g_interface_3_xaui_rx_l3_n_pin = nf10_10g_interface_2_xaui_tx_l3_n_pin;
+
+  end
+  
+  // Part 2: Reset
+  initial begin
+      RS232_Uart_1_sin = 1'b0;
+      CLK   = 1'b0;
+      
+      refclk_A_p = 1'b0;
+      refclk_A_n = 1'b1;
+      refclk_B_p = 1'b0;
+      refclk_B_n = 1'b1;
+      refclk_C_p = 1'b0;
+      refclk_C_n = 1'b1;      
+      refclk_D_p = 1'b0;
+      refclk_D_n = 1'b1;
+              
+      $display("[%t] : System Reset Asserted...", $realtime);
+      RESET = 1'b0;
+      for (i = 0; i < 50; i = i + 1) begin
+                 @(posedge CLK);
+      end
+      $display("[%t] : System Reset De-asserted...", $realtime);
+      RESET = 1'b1;
+  end
+  
+  // Part 3: Clock
+  always #5  CLK = ~CLK;      // 100MHz
+  always #3.2 refclk_A_p = ~refclk_A_p; // 156.25MHz
+  always #3.2 refclk_A_n = ~refclk_A_n; // 156.25MHz
+  always #3.2 refclk_B_p = ~refclk_B_p; // 156.25MHz
+  always #3.2 refclk_B_n = ~refclk_B_n; // 156.25MHz
+  always #3.2 refclk_C_p = ~refclk_C_p; // 156.25MHz
+  always #3.2 refclk_C_n = ~refclk_C_n; // 156.25MHz
+  always #3.2 refclk_D_p = ~refclk_D_p; // 156.25MHz
+  always #3.2 refclk_D_n = ~refclk_D_n; // 156.25MHz
+
+  // END USER CODE (Do not remove this line)
+
+endmodule
+
