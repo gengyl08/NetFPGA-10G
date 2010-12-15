e4d17baf87ee32c327d4edc1f04fc78641b65b6e
eastzone <hyzeng@stanford.edu>
2010-12-15 11:27:05 -0800

    nf10_axis_gen_check: fixed "last" signal actions

--- lib/hw/std/pcores/nf10_axis_gen_check_v1_00_a/hdl/vhdl/nf10_axis_gen_check.vhd
+++ lib/hw/std/pcores/nf10_axis_gen_check_v1_00_a/hdl/vhdl/nf10_axis_gen_check.vhd
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------
 --
---  NETFPGA-10G www.netfpga.org
+--  NetFPGA-10G http://www.netfpga.org
 --
 --  Module:
 --          nf10_axis_gen_check.vhd
@@ -9,13 +9,9 @@
 --          Hardware component that generates and checks packets
 --                 
 --  Revision history:
---          Dec 2010 M.Blott initial version
+--          2010/12/1  M.Blott  Initial version
+--          2010/12/15 hyzeng   Fixed last signal, AXI4-Lite
 --
---  Known issues:
---          none
---
---  Library: 
---          none beyond the standard
 ------------------------------------------------------------------------
 
 
@@ -26,15 +22,15 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity nf10_axis_gen_check is
 generic (
-   C_S_AXI_DATA_WIDTH  : integer range 32 to 32 := 32;
-   C_S_AXI_ADDR_WIDTH  : integer                := 32;
    C_BASEADDR          : std_logic_vector(31 downto 0) := x"00000000";
    C_HIGHADDR          : std_logic_vector(31 downto 0) := x"00000002";
    C_M_AXIS_DATA_WIDTH : integer := 64; -- max 256bit supported
    C_S_AXIS_DATA_WIDTH : integer := 64; -- max 256bit supported
-   GEN_PKT_SIZE        : integer := 10; -- in words; pkt_size + ifg_size < 65k=number of provisioned states  
-   CHECK_PKT_SIZE      : integer := 8; -- in words; pkt_size + ifg_size < 65k=number of provisioned states
-   IFG_SIZE            : integer := 5 -- in words irrespective of backpressure
+   C_GEN_PKT_SIZE      : integer := 10; -- in words; pkt_size + ifg_size < 65k=number of provisioned states  
+   C_CHECK_PKT_SIZE    : integer := 10; -- in words; pkt_size + ifg_size < 65k=number of provisioned states
+   C_IFG_SIZE          : integer := 5; -- in words irrespective of backpressure
+   C_S_AXI_ADDR_WIDTH  : integer := 32;
+   C_S_AXI_DATA_WIDTH  : integer := 32
 );
 port (
    ACLK               : in  std_logic;
@@ -51,6 +47,8 @@ port (
    S_AXIS_TREADY      : out std_logic;
    S_AXIS_TLAST       : in  std_logic;
    -- axi lite control/status interface
+   S_AXI_ACLK         : in  std_logic;
+   S_AXI_ARESETN      : in  std_logic;
    S_AXI_AWADDR       : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
    S_AXI_AWVALID      : in  std_logic;
    S_AXI_AWREADY      : out std_logic;
@@ -74,10 +72,10 @@ end nf10_axis_gen_check;
 
 architecture structural of nf10_axis_gen_check is
 
-   type gen_rom_type is array (0 to GEN_PKT_SIZE-1) of std_logic_vector (255 downto 0);--256bit maximum
-   type check_rom_type is array (0 to CHECK_PKT_SIZE-1) of std_logic_vector (255 downto 0);--256bit maximum
+   type gen_rom_type is array (0 to C_GEN_PKT_SIZE-1) of std_logic_vector (255 downto 0);--256bit maximum
+   type check_rom_type is array (0 to C_CHECK_PKT_SIZE-1) of std_logic_vector (255 downto 0);--256bit maximum
    -- in this example transmitted and received packets are identical
-   constant gen_ROM, check_ROM : gen_rom_type :=
+   constant gen_ROM : gen_rom_type :=
 	                          (x"0000000000000000000000000000000000000000000000000002cfbddde50005",
                                x"0000000000000000000000000000000000000000000000009a3c780008004500",
                                x"0000000000000000000000000000000000000000000000000040874c40008006",
@@ -88,6 +86,17 @@ architecture structural of nf10_axis_gen_check is
                                x"00000000000000000000000000000000000000000000000004ec010303020101",
                                x"000000000000000000000000000000000000000000000000080a000000000000",
                                x"0000000000000000000000000000000000000000000000000000010104020000");     
+   constant check_ROM : check_rom_type :=
+	                          (x"0000000000000000000000000000000000000000000000000002cfbddde50005",
+                               x"0000000000000000000000000000000000000000000000009a3c780008004500",
+                               x"0000000000000000000000000000000000000000000000000040874c40008006",
+                               x"000000000000000000000000000000000000000000000000450dac10e80ad02b",
+                               x"000000000000000000000000000000000000000000000000ca1705d90050c5e3",
+                               x"00000000000000000000000000000000000000000000000075ce00000000b002",
+                               x"000000000000000000000000000000000000000000000000ffffc68d00000204",
+                               x"00000000000000000000000000000000000000000000000004ec010303020101",
+                               x"000000000000000000000000000000000000000000000000080a000000000000",
+                               x"0000000000000000000000000000000000000000000000000000010104020000");
    -- ROM should be inferred as BRAM during XST
 
    constant AXI_RESP_OK          : std_logic_vector(1 downto 0) := "00";
@@ -99,15 +108,13 @@ architecture structural of nf10_axis_gen_check is
    signal tx_count           : std_logic_vector(31 downto 0);
    signal rx_count           : std_logic_vector(31 downto 0);
    signal err_count          : std_logic_vector(31 downto 0);
-   signal ok                 : std_logic;
-   signal last_data_plus_one : std_logic_vector(3 downto 0);	
+   signal ok                 : std_logic;	
    signal gen_rom_addr       : std_logic_vector(15 downto 0);
    signal check_rom_addr     : std_logic_vector(15 downto 0);
    signal pkt_data_buf       : std_logic_vector(C_S_AXIS_DATA_WIDTH-1 downto 0);
    signal pkt_valid_buf      : std_logic;
    signal pkt_last_buf       : std_logic;	
    signal pkt_strb_buf       : std_logic_vector(C_S_AXIS_DATA_WIDTH/8-1 downto 0);
-   signal last_seen          : std_logic;
    signal local_adr          : std_logic_vector(31 downto 0);
 	
 begin
@@ -129,7 +136,7 @@ begin
          if (M_AXIS_TREADY='1') then
             gen_state <= gen_state + 1;
          end if;
-      elsif gen_state < GEN_PKT_SIZE then 
+      elsif gen_state < C_GEN_PKT_SIZE then 
          for i in 0 to C_M_AXIS_DATA_WIDTH-1 loop
 			   M_AXIS_TDATA(i) <= gen_ROM(conv_integer(gen_rom_addr))(i);
          end loop;
@@ -139,7 +146,7 @@ begin
          if (M_AXIS_TREADY='1') then
             gen_state <= gen_state + 1;
          end if;	
-      elsif gen_state = GEN_PKT_SIZE then
+      elsif gen_state = C_GEN_PKT_SIZE then
          for i in 0 to C_M_AXIS_DATA_WIDTH-1 loop
 			   M_AXIS_TDATA(i) <= gen_ROM(conv_integer(gen_rom_addr))(i);
          end loop;
@@ -150,7 +157,7 @@ begin
             tx_count <= tx_count + 1;	
             gen_state <= gen_state + 1;
          end if;	
-      elsif gen_state < GEN_PKT_SIZE+IFG_SIZE-1 then
+      elsif gen_state < C_GEN_PKT_SIZE+C_IFG_SIZE-1 then
          M_AXIS_TDATA <= (others => '0');
          M_AXIS_TSTRB <= (others => '0');
          M_AXIS_TVALID <= '0';
@@ -170,21 +177,20 @@ S_AXIS_TREADY <= '1';
 check_p: process(ACLK)
 begin
    if (ARESETN='0') then
-      check_state <= (others => '0');
-      rx_count <= (others => '0');
-		err_count <= (others => '0');
-      ok <= '1';
+        check_state <= (others => '0');
+        rx_count <= (others => '0');
+        err_count <= (others => '0');
+        ok <= '1';
 		check_rom_addr <= x"0000"; 
 		pkt_data_buf <= (others => '0');
-      pkt_valid_buf <= '0';
+        pkt_valid_buf <= '0';
 		pkt_last_buf <= '0';
 		pkt_strb_buf <= (others => '0');
-      last_seen <= '0';
    elsif (ACLK = '1' and ACLK'event) then
-	   pkt_data_buf <= S_AXIS_TDATA;
-		pkt_valid_buf <= S_AXIS_TVALID;
-		pkt_last_buf <= S_AXIS_TLAST;
-		pkt_strb_buf <= S_AXIS_TSTRB;
+	  pkt_data_buf <= S_AXIS_TDATA;
+      pkt_valid_buf <= S_AXIS_TVALID;
+      pkt_last_buf <= S_AXIS_TLAST;
+      pkt_strb_buf <= S_AXIS_TSTRB;
       if check_state = x"0000" then
          -- waiting for a pkt
          if S_AXIS_TVALID = '1' then
@@ -192,10 +198,9 @@ begin
             check_rom_addr <= x"0000"; 
             check_state <= check_state + 1;
          end if;
-			last_seen <= '0';
       elsif check_state = x"0001" then
-		  -- strb checking needs to be added
-		   -- checking the packet against ROM
+		 -- strb checking needs to be added
+		 -- checking the packet against ROM
          for i in 0 to C_M_AXIS_DATA_WIDTH-1 loop   
            if (pkt_data_buf(i) = check_ROM(conv_integer(check_rom_addr))(i) and pkt_strb_buf(i/8) = '1') then
                ok <= ok;
@@ -204,32 +209,40 @@ begin
             end if; 
          end loop;
          -- check packet size and last
-         if (check_rom_addr = CHECK_PKT_SIZE -1 and pkt_last_buf= '1') then
-			   last_seen <= '1';
-		      check_state <= check_state + 1;
-			elsif (check_rom_addr = CHECK_PKT_SIZE -1 and pkt_last_buf = '0') then
-			   last_seen <= '0';
-		      check_state <= check_state + 1;
-		   elsif (check_rom_addr < CHECK_PKT_SIZE -1 and pkt_last_buf = '1') then
-            check_state <= check_state + 1;
-         end if;		    
-		   check_rom_addr <= check_rom_addr + 1;	
+         if (check_rom_addr = C_CHECK_PKT_SIZE -1) then
+		      if (pkt_last_buf='1') then
+		          check_state <= x"0002"; -- finish up
+		      else
+		          check_state <= x"0003"; -- Wait for last
+		      end if;
+         end if;
+         if (pkt_valid_buf = '1') then		    
+		     check_rom_addr <= check_rom_addr + 1;
+		 end if;	
       elsif check_state = x"0002" then
-          -- finish up
-         if (ok='1' and (last_seen='1' or pkt_last_buf='1')) then
-			   rx_count <= rx_count + 1;
-				check_state <= (others => '0'); 	
-			else
-			   err_count <= err_count + 1;
-				check_state <= (others => '0'); 	
-			end if;
+         -- finish up
+         if (ok='1') then
+			rx_count <= rx_count + 1;
+		 else
+			err_count <= err_count + 1;
+		 end if;
+		 check_state <= (others => '0'); 
+		 ok <='1';
+      elsif check_state = x"0003" then
+         -- Wait for last
+         if (pkt_valid_buf = '1') then  -- No more words!
+			ok <= '0';
+	     end if;
+	     if (pkt_last_buf='1') then
+		    check_state <= x"0002"; 
+		 end if;
       end if;
    end if;
 end process;
 
-axi_lite_if_p: process(ACLK)
+axi_lite_if_p: process(S_AXI_ACLK)
 begin
-   if (ARESETN='0') then
+   if (S_AXI_ARESETN='0') then
       S_AXI_ARREADY <= '0';
       S_AXI_AWREADY <= '0';
       S_AXI_RVALID <= '0';
@@ -238,7 +251,7 @@ begin
       S_AXI_WREADY <= '0';
       S_AXI_BRESP <= AXI_RESP_OK;
       S_AXI_BVALID <= '0';
-   elsif (ACLK'event and ACLK='1') then
+   elsif (S_AXI_ACLK'event and S_AXI_ACLK='1') then
       -- defaults
       S_AXI_ARREADY <= '0';
       S_AXI_AWREADY <= '0';
@@ -248,6 +261,7 @@ begin
       S_AXI_WREADY <= '0';
       S_AXI_BRESP <= AXI_RESP_OK;
       S_AXI_BVALID <= '0';
+      local_adr <= (others => '0');
       -- get read address and acknowledge
       if S_AXI_ARVALID = '1' then
          local_adr <= S_AXI_ARADDR; 
