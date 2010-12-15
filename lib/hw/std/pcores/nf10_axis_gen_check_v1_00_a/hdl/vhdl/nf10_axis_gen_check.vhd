3cca96ef504f31b70095f824b2a2c84c91e483d7
Michaela Blott <mblott@xirlsgp1.(none)>
2010-12-15 16:23:23 +0000

    added axi-lite interface

--- lib/hw/std/pcores/nf10_axis_gen_check_v1_00_a/hdl/vhdl/nf10_axis_gen_check.vhd
+++ lib/hw/std/pcores/nf10_axis_gen_check_v1_00_a/hdl/vhdl/nf10_axis_gen_check.vhd
@@ -26,15 +26,20 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity nf10_axis_gen_check is
 generic (
+   C_S_AXI_DATA_WIDTH  : integer range 32 to 32 := 32;
+   C_S_AXI_ADDR_WIDTH  : integer                := 32;
+   C_BASEADDR          : std_logic_vector(31 downto 0) := x"00000000";
+   C_HIGHADDR          : std_logic_vector(31 downto 0) := x"00000002";
    C_M_AXIS_DATA_WIDTH : integer := 64; -- max 256bit supported
    C_S_AXIS_DATA_WIDTH : integer := 64; -- max 256bit supported
    GEN_PKT_SIZE        : integer := 10; -- in words; pkt_size + ifg_size < 65k=number of provisioned states  
-   CHECK_PKT_SIZE      : integer := 10; -- in words; pkt_size + ifg_size < 65k=number of provisioned states
+   CHECK_PKT_SIZE      : integer := 8; -- in words; pkt_size + ifg_size < 65k=number of provisioned states
    IFG_SIZE            : integer := 5 -- in words irrespective of backpressure
 );
 port (
    ACLK               : in  std_logic;
    ARESETN            : in  std_logic;
+   -- axi streaming data interface
    M_AXIS_TDATA       : out std_logic_vector (C_M_AXIS_DATA_WIDTH-1 downto 0);
    M_AXIS_TSTRB       : out std_logic_vector (C_M_AXIS_DATA_WIDTH/8-1 downto 0);
    M_AXIS_TVALID      : out std_logic;
@@ -44,7 +49,25 @@ port (
    S_AXIS_TSTRB       : in  std_logic_vector (C_S_AXIS_DATA_WIDTH/8-1 downto 0);
    S_AXIS_TVALID      : in  std_logic;
    S_AXIS_TREADY      : out std_logic;
-   S_AXIS_TLAST       : in  std_logic		
+   S_AXIS_TLAST       : in  std_logic;
+   -- axi lite control/status interface
+   S_AXI_AWADDR       : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+   S_AXI_AWVALID      : in  std_logic;
+   S_AXI_AWREADY      : out std_logic;
+   S_AXI_WDATA        : in  std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+   S_AXI_WSTRB        : in  std_logic_vector((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+   S_AXI_WVALID       : in  std_logic;
+   S_AXI_WREADY       : out std_logic;
+   S_AXI_BRESP        : out std_logic_vector(1 downto 0);
+   S_AXI_BVALID       : out std_logic;
+   S_AXI_BREADY       : in  std_logic;
+   S_AXI_ARADDR       : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+   S_AXI_ARVALID      : in  std_logic;
+   S_AXI_ARREADY      : out std_logic;
+   S_AXI_RDATA        : out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+   S_AXI_RRESP        : out std_logic_vector(1 downto 0);
+   S_AXI_RVALID       : out std_logic;
+   S_AXI_RREADY       : in  std_logic	
 );
 end nf10_axis_gen_check;
 
@@ -66,6 +89,10 @@ architecture structural of nf10_axis_gen_check is
                                x"000000000000000000000000000000000000000000000000080a000000000000",
                                x"0000000000000000000000000000000000000000000000000000010104020000");     
    -- ROM should be inferred as BRAM during XST
+
+   constant AXI_RESP_OK          : std_logic_vector(1 downto 0) := "00";
+   constant AXI_RESP_SLVERR      : std_logic_vector(1 downto 0) := "10";
+   constant AXI_RESP_DECERR      : std_logic_vector(1 downto 0) := "11";
 	
    signal gen_state          : std_logic_vector(15 downto 0);
    signal check_state        : std_logic_vector(15 downto 0);
@@ -79,8 +106,9 @@ architecture structural of nf10_axis_gen_check is
    signal pkt_data_buf       : std_logic_vector(C_S_AXIS_DATA_WIDTH-1 downto 0);
    signal pkt_valid_buf      : std_logic;
    signal pkt_last_buf       : std_logic;	
-	signal pkt_strb_buf       : std_logic_vector(C_S_AXIS_DATA_WIDTH/8-1 downto 0);
+   signal pkt_strb_buf       : std_logic_vector(C_S_AXIS_DATA_WIDTH/8-1 downto 0);
    signal last_seen          : std_logic;
+   signal local_adr          : std_logic_vector(31 downto 0);
 	
 begin
 
@@ -198,5 +226,65 @@ begin
       end if;
    end if;
 end process;
+
+axi_lite_if_p: process(ACLK)
+begin
+   if (ARESETN='0') then
+      S_AXI_ARREADY <= '0';
+      S_AXI_AWREADY <= '0';
+      S_AXI_RVALID <= '0';
+      S_AXI_RRESP <= AXI_RESP_OK;
+      S_AXI_RDATA <= (others => '0');
+      S_AXI_WREADY <= '0';
+      S_AXI_BRESP <= AXI_RESP_OK;
+      S_AXI_BVALID <= '0';
+   elsif (ACLK'event and ACLK='1') then
+      -- defaults
+      S_AXI_ARREADY <= '0';
+      S_AXI_AWREADY <= '0';
+      S_AXI_RVALID <= '0';
+      S_AXI_RRESP <= AXI_RESP_OK;
+      S_AXI_RDATA <= (others => '0');
+      S_AXI_WREADY <= '0';
+      S_AXI_BRESP <= AXI_RESP_OK;
+      S_AXI_BVALID <= '0';
+      -- get read address and acknowledge
+      if S_AXI_ARVALID = '1' then
+         local_adr <= S_AXI_ARADDR; 
+         S_AXI_ARREADY <= '1';
+      end if;
+      -- read response channel
+      if S_AXI_RREADY = '1' then
+         S_AXI_RVALID <= '1';
+      end if;
+      if S_AXI_RREADY = '1' and local_adr = C_BASEADDR then
+         S_AXI_RDATA <= tx_count;
+         S_AXI_RRESP <= AXI_RESP_OK;
+      elsif S_AXI_RREADY = '1' and local_adr = C_BASEADDR+1 then
+         S_AXI_RDATA <= rx_count;
+         S_AXI_RRESP <= AXI_RESP_OK;
+      elsif S_AXI_RREADY = '1' and local_adr = C_BASEADDR+2 then
+         S_AXI_RDATA <= err_count;
+         S_AXI_RRESP <= AXI_RESP_OK;
+      elsif S_AXI_RREADY = '1' then
+         -- address out of range
+         S_AXI_RRESP <= AXI_RESP_SLVERR; -- right error type?
+      end if;
+      -- write address - no write supported, just ignore inputs but ack
+      if S_AXI_AWVALID = '1' then
+         S_AXI_AWREADY <= '1';
+      end if;
+      -- write data channel -- ignore data and strobes
+      if S_AXI_WVALID = '1' then
+         S_AXI_WREADY <= '1';
+      end if;
+      -- write response channel -- send error back
+      if S_AXI_BREADY = '1' then
+         S_AXI_BRESP <= AXI_RESP_SLVERR;
+         S_AXI_BVALID <= '1';
+      end if;
+   end if;
+end process;
+
 
 end structural;
