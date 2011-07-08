cc3418f6b80835d8a4d484be504a127c4840c5d2
Mark Grindall <mgrinda@xirlsgp1.(none)>
2011-07-08 15:43:08 +0100

    removing accidental checking

--- projects/configuration_test/CPLD/FlashControllerTB/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "/home/mgrinda/netfpga_10g_dev_WORKING_BOOT/projects/configuration_test/CPLD/FlashControllerTB/FlashTest_isim_beh.exe" -prj "/home/mgrinda/netfpga_10g_dev_WORKING_BOOT/projects/configuration_test/CPLD/FlashControllerTB/FlashTest_beh.prj" "work.FlashTest" 
