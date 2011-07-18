c46e86e852e435e6e0adb77e6ff6549ed89a82c9
Mark Grindall <mgrinda@xirlsgp1.(none)>
2011-07-18 12:05:43 +0100

    added selfchecking testbenches

---
+++ lib/hw/std/pcores/nf10_nic_output_port_lookup_v1_00_a/hdl/verilog/nf10_nic_output_port_lookup_tb.sh
@@ -0,0 +1,2 @@
+fuse -incremental -prj nf10_nic_output_port_lookup_tb.prj -o nf10_nic_output_port_lookup_tb.exe testbench
+nf10_nic_output_port_lookup_tb.exe -tclbatch nf10_nic_output_port_lookup_tb.tcl
