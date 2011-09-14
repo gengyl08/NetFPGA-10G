efb05cb1a851eb33e95d7a551337b1c424038ecc
David J. Miller <david.miller@cl.cam.ac.uk>
2011-09-14 15:25:25 +0100

    untabify; interpreter uses 'env'; and minor enhancements to link-py-interpreter
    link-py-interpreter.sh:
    	- add support for 'python2.X' etc
    	- do nothing if target link already exists

--- tools/scripts/link-py-interpreter.sh
+++ tools/scripts/link-py-interpreter.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ###########################################################################
 #
@@ -13,22 +13,31 @@
 #  Description:
 #       Locate and create a link to a suitable python interpreter.
 #
-#	Mostly, this is to accommodate older distributions like CentOS,
-#	which do provide sufficiently recent installations of python - but
-#	not with the standard name, 'python'.
+#       Mostly, this is to accommodate older distributions like CentOS,
+#       which do provide sufficiently recent installations of python - but
+#       not with the standard name, 'python'.
 #
 #
 
-for py in python27 python26 python25 python; do
+target=$(dirname $0)/python-nf
+
+# Do nothing if link already exists
+if [ -x $target ]; then
+    echo $target exists: leaving alone.
+    exit 0
+fi
+
+# Find and link suitable interpreter
+for py in python27 python2.7 python26 python2.6 python25 python2.5 python; do
     if py=$(which $py 2>/dev/null); then
-	echo Found python interpreter: $py
-	if ! $py -c "import sys; sys.exit( sys.version_info[:2] < (2,5) )"; then
-	    echo $py: too old
-	else
-	    echo ln -sf $py $(dirname $0)/python-nf
-	    ln -sf $py $(dirname $0)/python-nf
-	    exit 0
-	fi
+        echo Found python interpreter: $py
+        if ! $py -c "import sys; sys.exit( sys.version_info[:2] < (2,5) )"; then
+            echo $py: too old
+        else
+            echo ln -s $py $target
+            ln -s $py $target
+            exit 0
+        fi
     fi
 done
 
--- tools/scripts/pci_save_restore.sh
+++ tools/scripts/pci_save_restore.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ###########################################################################
 #
@@ -21,14 +21,14 @@
 #       the logical PCI bus ID.  Use lspci -d 10ee: to identify bus IDs.
 #
 
-sbp=/sys/bus/pci	  # sysfs PCI subtree
+sbp=/sys/bus/pci          # sysfs PCI subtree
 sf=/tmp/pci-config-save   # PCI config space save file base name
-vid=0x10ee		  # nf10 PCI vendor ID
-pid=0x4243		  # nf10 PCI product ID
+vid=0x10ee                # nf10 PCI vendor ID
+pid=0x4243                # nf10 PCI product ID
 
 help () {
-	echo 'syntax: save|restore [target_bus_id]'
-	exit 0
+        echo 'syntax: save|restore [target_bus_id]'
+        exit 0
 }
 
 
@@ -40,43 +40,43 @@ config=$2
 
 # sanity check sysfs
 if [ ! -d $sbp ]; then
-	echo "sysfs apparently not mounted: $sbp doesn't exist!"
-	exit 1
+        echo "sysfs apparently not mounted: $sbp doesn't exist!"
+        exit 1
 fi
 
 # elaborate path to PCI config space
 if [ -z "$config" ]; then
-	# automatically detect - find first NF10 card
-	for each in $sbp/devices/0000\:*\:00.0; do
-		if [ $(($(cat $each/vendor))) -eq $(($vid)) -a \
-		   $(($(cat $each/device))) -eq $(($pid)) ]; then
-			config=$each
-		fi
-	done
-	if [ -z "$config" ]; then
-		echo "no netfpga found!"
-		exit 1
-	fi
+        # automatically detect - find first NF10 card
+        for each in $sbp/devices/0000\:*\:00.0; do
+                if [ $(($(cat $each/vendor))) -eq $(($vid)) -a \
+                   $(($(cat $each/device))) -eq $(($pid)) ]; then
+                        config=$each
+                fi
+        done
+        if [ -z "$config" ]; then
+                echo "no netfpga found!"
+                exit 1
+        fi
 else
-	config=$sbp/devices/0000:$(printf "%02x" $config):00.0
+        config=$sbp/devices/0000:$(printf "%02x" $config):00.0
 fi
 sf=$sf-$(echo $config|cut -f2- -d:)
 config=$config/config
 
 # sanity check access
 if [ ! -f $config ]; then
-	echo "can't access $config"
-	exit 1
+        echo "can't access $config"
+        exit 1
 fi
 
 case $op in
-	s*) 
-	cp -v $config $sf
-	;;
-	r*) 
-	cp -v $sf $config
-	;;
-	*)
-	help
-	;;
+        s*)
+        cp -v $config $sf
+        ;;
+        r*)
+        cp -v $sf $config
+        ;;
+        *)
+        help
+        ;;
 esac
