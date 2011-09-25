f2c3edba8e3f3146e5232772b98d95e0d5c5aa40
Jonathan Ellithorpe <jdellit@stanford.edu>
2011-09-24 19:31:36 -0700

    Updating driver code

--- projects/reference_nic/sw/host/driver/src/nf10_reg_lib.h
+++ projects/reference_nic/sw/host/driver/src/nf10_reg_lib.h
@@ -1,41 +1,15 @@
-/*******************************************************************************
+/* NetFPGA-10G http://www.netfpga.org
+ * 
+ * NetFPGA-10G Register Access Library Header File
  *
- *  NetFPGA-10G http://www.netfpga.org
+ * Description:
+ *  Set of definitions for the NF10 register access library.
  *
- *  File:
- *        nf10_reg_lib.h
- *
- *  Project:
- *        reference_nic
- *
- *  Author:
- *        James Hongyi Zeng
- *
- *  Description:
- *        Set of definitions for the NF10 register access library.
- *
- *  Copyright notice:
- *        Copyright (C) 2010,2011 The Board of Trustees of The Leland Stanford
- *                                Junior University
- *
- *  Licence:
- *        This file is part of the NetFPGA 10G development base package.
- *
- *        This package is free software: you can redistribute it and/or modify
- *        it under the terms of the GNU Lesser General Public License as
- *        published by the Free Software Foundation, either version 3 of the
- *        License, or (at your option) any later version.
- *
- *        This package is distributed in the hope that it will be useful, but
- *        WITHOUT ANY WARRANTY; without even the implied warranty of
- *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *        Lesser General Public License for more details.
- *
- *        You should have received a copy of the GNU Lesser General Public
- *        License along with the NetFPGA source package.  If not, see
- *        http://www.gnu.org/licenses/.
- *
+ * Revision history: 
+ *  2011/07/06 Jonathan Ellithorpe: Created.
  */
 
+
+/* Function prototypes. */
 int         nf10_reg_rd(uint32_t addr, uint32_t *val_ptr);
 int         nf10_reg_wr(uint32_t addr, uint32_t val);
