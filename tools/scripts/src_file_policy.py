625112d943cb19ee8cbb4f734c1324734a840d1a
David J. Miller <david.miller@cl.cam.ac.uk>
2011-10-04 19:58:07 +0100

    Change header licence: 	- to LGPL 2.1, in tentative anticipation of the decision to use 	  LGPL 2.1 (as agreed with Kees in -dev call of 30 September) 	- Only LGPL 2.1 - no choice to use a later version, since 	  it's clear that LGPL 3 is problematic 	- change reference 'this package' to 'this file', since not 	  everything in the package is covered by the same licence.

--- tools/scripts/src_file_policy.py
+++ tools/scripts/src_file_policy.py
@@ -169,10 +169,10 @@ Copyright (C) 2010,2011 The Board of Trustees of The Leland Stanford
 stanford_licence = """\
 This file is part of the NetFPGA 10G development base package.
 
-This package is free software: you can redistribute it and/or modify
+This file is free code: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+published by the Free Software Foundation version 2.1 of the
+License.
 
 This package is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
