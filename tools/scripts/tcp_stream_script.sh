a3ed098f6c7431880f1a3ab0a88d579894e632cf
Muhammad Shahbaz <ms2035@cl.cam.ac.uk>
2012-03-13 19:56:51 +0000

    Updated header information on performance test scripts

--- tools/scripts/tcp_stream_script.sh
+++ tools/scripts/tcp_stream_script.sh
@@ -1,14 +1,38 @@
 #!/bin/sh
+
+################################################################################
 #
-# This is an example script for using netperf. Feel free to modify it 
-# as necessary, but I would suggest that you copy this one first.
-# 
-# This version has been modified to take advantage of the confidence
-# interval support in revision 2.0 of netperf. it has also been altered
-# to make submitting its resutls to the netperf database easier
-# raj 11/94
+#  NetFPGA-10G http://www.netfpga.org
 #
-# usage: tcp_stream_script hostname [CPU]
+#  File:
+#        tcp_stream_script.sh
+#
+#  Author:
+#        Muhammad Shahbaz
+#
+#  Description:
+#        TCP performance test script.
+#
+#  Copyright notice:
+#        Copyright (C) 2010, 2011 The Board of Trustees of The Leland Stanford
+#                                 Junior University
+#
+#  Licence:
+#        This file is part of the NetFPGA 10G development base package.
+#
+#        This file is free code: you can redistribute it and/or modify it under
+#        the terms of the GNU Lesser General Public License version 2.1 as
+#        published by the Free Software Foundation.
+#
+#        This package is distributed in the hope that it will be useful, but
+#        WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#        Lesser General Public License for more details.
+#
+#        You should have received a copy of the GNU Lesser General Public
+#        License along with the NetFPGA source package.  If not, see
+#        http://www.gnu.org/licenses/.
+#
 #
 
 if [ $# -gt 4 ]; then
--- tools/scripts/udp_stream_script.sh
+++ tools/scripts/udp_stream_script.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
+
+################################################################################
 #
-# This is an example script for using netperf. Feel free to modify it 
-# as necessary, but I would suggest that you copy this one first.
-# This script performs various UDP unidirectional stream tests.
+#  NetFPGA-10G http://www.netfpga.org
+#
+#  File:
+#        udp_stream_script.sh
+#
+#  Author:
+#        Muhammad Shahbaz
+#
+#  Description:
+#        UDP performance test script.
+#
+#  Copyright notice:
+#        Copyright (C) 2010, 2011 The Board of Trustees of The Leland Stanford
+#                                 Junior University
+#
+#  Licence:
+#        This file is part of the NetFPGA 10G development base package.
+#
+#        This file is free code: you can redistribute it and/or modify it under
+#        the terms of the GNU Lesser General Public License version 2.1 as
+#        published by the Free Software Foundation.
+#
+#        This package is distributed in the hope that it will be useful, but
+#        WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#        Lesser General Public License for more details.
+#
+#        You should have received a copy of the GNU Lesser General Public
+#        License along with the NetFPGA source package.  If not, see
+#        http://www.gnu.org/licenses/.
+#
 #
 
 if [ $# -gt 4 ]; then
