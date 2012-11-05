c4f4c9eb24df4051e3707745bbe5863186b27b30
mshahbaz <muhammad.shahbaz@cl.cam.ac.uk>
2012-11-05 12:39:48 +0000

    Updated nf10_reg_io_lib.c

--- lib/sw/contrib/drivers/nf10_reg_io_v1_00_a/src/nf10_reg_io_lib.c
+++ lib/sw/contrib/drivers/nf10_reg_io_v1_00_a/src/nf10_reg_io_lib.c
@@ -47,116 +47,112 @@
 
 void run(int dev)
 {
+	printf("--- Registers Test ---\r\n\n");
 	reg_wr(dev, BAR0_BASE_ADDRESS + WO_REG0, 0xdeed0001);
-	printf("Write WO_REG0 (%x)\r\n", 0xdeed0001);
+	printf("Write WO_REG0 \t (%x)\r\n", 0xdeed0001);
 	reg_wr(dev, BAR0_BASE_ADDRESS + WO_REG1, 0xdeed0002);
-	printf("Write WO_REG1 (%x)\r\n", 0xdeed0002);
+	printf("Write WO_REG1 \t (%x)\r\n\n", 0xdeed0002);
 	reg_wr(dev, BAR0_BASE_ADDRESS + RW_REG0, 0xdeed1003);
-	printf("Write RW_REG0 (%x)\r\n", 0xdeed1003);
+	printf("Write RW_REG0 \t (%x)\r\n", 0xdeed1003);
 	reg_wr(dev, BAR0_BASE_ADDRESS + RW_REG1, 0xdeed1004);
-	printf("Write RW_REG1 (%x)\r\n", 0xdeed1004);
+	printf("Write RW_REG1 \t (%x)\r\n\n", 0xdeed1004);
 
-	printf("Read WO_REG0 (%x) == 0\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + WO_REG0));
-	printf("Read WO_REG1 (%x) == 0\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + WO_REG1));
-	printf("Read RW_REG0 (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RW_REG0));
-	printf("Read RW_REG1 (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RW_REG1));
-	printf("Read RO_REG0 (%x) == RW_REG0 (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RO_REG0), reg_rd(dev, BAR0_BASE_ADDRESS + RW_REG0));
-	printf("Read RO_REG1 (%x) == RW_REG1 (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RO_REG1), reg_rd(dev, BAR0_BASE_ADDRESS + RW_REG1));
-	printf("Read RO_REG2 (%x) == W0_REG0 (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RO_REG2), reg_rd(dev, BAR0_BASE_ADDRESS + WO_REG0));
-	printf("Read RO_REG3 (%x) == W0_REG1 (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RO_REG3), reg_rd(dev, BAR0_BASE_ADDRESS + WO_REG1));
+	printf("Read WO_REG0 \t (%x) == 0\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + WO_REG0));
+	printf("Read WO_REG1 \t (%x) == 0\r\n\n", reg_rd(dev, BAR0_BASE_ADDRESS + WO_REG1));
+	
+	printf("Read RW_REG0 \t (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RW_REG0));
+	printf("Read RW_REG1 \t (%x)\r\n\n", reg_rd(dev, BAR0_BASE_ADDRESS + RW_REG1));
 
-	printf("\n\n\n\r");
+	printf("Read RO_REG0 \t (%x) == RW_REG0 \t (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RO_REG0), reg_rd(dev, BAR0_BASE_ADDRESS + RW_REG0));
+	printf("Read RO_REG1 \t (%x) == RW_REG1 \t (%x)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RO_REG1), reg_rd(dev, BAR0_BASE_ADDRESS + RW_REG1));
+	printf("Read RO_REG2 \t (%x) == W0_REG0 \t (0xdeed0001)\r\n", reg_rd(dev, BAR0_BASE_ADDRESS + RO_REG2));
+	printf("Read RO_REG3 \t (%x) == W0_REG1 \t (0xdeed0002)\r\n\n", reg_rd(dev, BAR0_BASE_ADDRESS + RO_REG3));
 
+	printf("\n\r");
+
+	printf("--- Table Test ---\r\n");
         // Write Row 0
-        printf("--- Row 0 ---");
+        printf("------ Row 0 ---\r\n");
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL0, 0xfeed0001);
-	printf("Write TBL_COL0 (%x)\r\n", 0xfeed0001);
+	printf("Write TBL_COL0 \t (%x)\r\n", 0xfeed0001);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL1, 0xfeed0002);
-	printf("Write TBL_COL1 (%x)\r\n", 0xfeed0002);
+	printf("Write TBL_COL1 \t (%x)\r\n", 0xfeed0002);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL2, 0xfeed0003);
-	printf("Write TBL_COL2 (%x)\r\n", 0xfeed0003);
+	printf("Write TBL_COL2 \t (%x)\r\n", 0xfeed0003);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL3, 0xfeed0004);
-	printf("Write TBL_COL3 (%x)\r\n", 0xfeed0004);
+	printf("Write TBL_COL3 \t (%x)\r\n", 0xfeed0004);
         reg_wr(dev, BAR1_BASE_ADDRESS + TBL_WR_ADDR, 0);
-	printf("Write TBL_WR_ADDR (%x)\r\n", 0);
-
-	printf("\r\n");
+	printf("Write TBL_WR_ADDR (%x)\r\n\n", 0);
 
 	// Read Row 0
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_RD_ADDR, 0);
 	printf("Write TBL_RD_ADDR (%x)\r\n", 0);
-	printf("Read TBL_COL0 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL0));
-	printf("Read TBL_COL1 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL1));
-	printf("Read TBL_COL2 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL2));
-	printf("Read TBL_COL3 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL3));
+	printf("Read TBL_COL0 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL0));
+	printf("Read TBL_COL1 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL1));
+	printf("Read TBL_COL2 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL2));
+	printf("Read TBL_COL3 \t (%x)\r\n\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL3));
 
 	// Write Row 1
-        printf("--- Row 1 ---");
-	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL1, 0xfeed1111);
-	printf("Write TBL_COL0 (%x)\r\n", 0xfeed1111);
+        printf("------ Row 1 ---\r\n");
+	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL0, 0xfeed1111);
+	printf("Write TBL_COL0 \t (%x)\r\n", 0xfeed1111);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL1, 0xfeed1112);
-	printf("Write TBL_COL1 (%x)\r\n", 0xfeed1112);
+	printf("Write TBL_COL1 \t (%x)\r\n", 0xfeed1112);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL2, 0xfeed1113);
-	printf("Write TBL_COL2 (%x)\r\n", 0xfeed1113);
+	printf("Write TBL_COL2 \t (%x)\r\n", 0xfeed1113);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL3, 0xfeed1114);
-	printf("Write TBL_COL3 (%x)\r\n", 0xfeed1114);
+	printf("Write TBL_COL3 \t (%x)\r\n", 0xfeed1114);
         reg_wr(dev, BAR1_BASE_ADDRESS + TBL_WR_ADDR, 1);
-	printf("Write TBL_WR_ADDR (%x)\r\n", 1);
-
-	printf("\r\n");
+	printf("Write TBL_WR_ADDR (%x)\r\n\n", 1);
 
 	// Read Row 1
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_RD_ADDR, 1);
 	printf("Write TBL_RD_ADDR (%x)\r\n", 1);
-	printf("Read TBL_COL0 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL0));
-	printf("Read TBL_COL1 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL1));
-	printf("Read TBL_COL2 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL2));
-	printf("Read TBL_COL3 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL3));
-        
-	// Write Row 2
-        printf("--- Row 2 ---");
-	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL1, 0xfeed2221);
-	printf("Write TBL_COL0 (%x)\r\n", 0xfeed2221);
-	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL1, 0xfeed2222);
-	printf("Write TBL_COL1 (%x)\r\n", 0xfeed2222);
-	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL2, 0xfeed2223);
-	printf("Write TBL_COL2 (%x)\r\n", 0xfeed2223);
-	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL3, 0xfeed2224);
-	printf("Write TBL_COL3 (%x)\r\n", 0xfeed2224);
-        reg_wr(dev, BAR1_BASE_ADDRESS + TBL_WR_ADDR, 2);
-	printf("Write TBL_WR_ADDR (%x)\r\n", 2);
+	printf("Read TBL_COL0 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL0));
+	printf("Read TBL_COL1 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL1));
+	printf("Read TBL_COL2 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL2));
+	printf("Read TBL_COL3 \t (%x)\r\n\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL3));
 
-	printf("\r\n");
+	// Write Row 2
+        printf("------ Row 2 ---\r\n");
+	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL0, 0xfeed2221);
+	printf("Write TBL_COL0 \t (%x)\r\n", 0xfeed2221);
+	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL1, 0xfeed2222);
+	printf("Write TBL_COL1 \t (%x)\r\n", 0xfeed2222);
+	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL2, 0xfeed2223);
+	printf("Write TBL_COL2 \t (%x)\r\n", 0xfeed2223);
+	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL3, 0xfeed2224);
+	printf("Write TBL_COL3 \t (%x)\r\n", 0xfeed2224);
+        reg_wr(dev, BAR1_BASE_ADDRESS + TBL_WR_ADDR, 2);
+	printf("Write TBL_WR_ADDR (%x)\r\n\n", 2);
 
 	// Read Row 2
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_RD_ADDR, 2);
 	printf("Write TBL_RD_ADDR (%x)\r\n", 2);
-	printf("Read TBL_COL0 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL0));
-	printf("Read TBL_COL1 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL1));
-	printf("Read TBL_COL2 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL2));
-	printf("Read TBL_COL3 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL3));
+	printf("Read TBL_COL0 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL0));
+	printf("Read TBL_COL1 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL1));
+	printf("Read TBL_COL2 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL2));
+	printf("Read TBL_COL3 \t (%x)\r\n\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL3));
 
 	// Write Row 3
-        printf("--- Row 3 ---");
-	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL1, 0xfeed3331);
-	printf("Write TBL_COL0 (%x)\r\n", 0xfeed3331);
+        printf("------ Row 3 ---\r\n");
+	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL0, 0xfeed3331);
+	printf("Write TBL_COL0 \t (%x)\r\n", 0xfeed3331);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL1, 0xfeed3332);
-	printf("Write TBL_COL1 (%x)\r\n", 0xfeed3332);
+	printf("Write TBL_COL1 \t (%x)\r\n", 0xfeed3332);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL2, 0xfeed3333);
-	printf("Write TBL_COL2 (%x)\r\n", 0xfeed3333);
+	printf("Write TBL_COL2 \t (%x)\r\n", 0xfeed3333);
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_COL3, 0xfeed3334);
-	printf("Write TBL_COL3 (%x)\r\n", 0xfeed3334);
+	printf("Write TBL_COL3 \t (%x)\r\n", 0xfeed3334);
         reg_wr(dev, BAR1_BASE_ADDRESS + TBL_WR_ADDR, 3);
-	printf("Write TBL_WR_ADDR (%x)\r\n", 3);
-
-	printf("\r\n");
+	printf("Write TBL_WR_ADDR (%x)\r\n\n", 3);
 
 	// Read Row 3
 	reg_wr(dev, BAR1_BASE_ADDRESS + TBL_RD_ADDR, 3);
 	printf("Write TBL_RD_ADDR (%x)\r\n", 3);
-	printf("Read TBL_COL0 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL0));
-	printf("Read TBL_COL1 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL1));
-	printf("Read TBL_COL2 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL2));
-	printf("Read TBL_COL3 (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL3));
+	printf("Read TBL_COL0 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL0));
+	printf("Read TBL_COL1 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL1));
+	printf("Read TBL_COL2 \t (%x)\r\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL2));
+	printf("Read TBL_COL3 \t (%x)\r\n\n", reg_rd(dev, BAR1_BASE_ADDRESS + TBL_COL3));
 }
 
