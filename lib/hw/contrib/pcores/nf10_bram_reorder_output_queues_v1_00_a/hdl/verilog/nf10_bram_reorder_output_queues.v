3a58b2f9405f4ec448231969aa9280096e313610
Yilong Geng <gengyl08@stanford.edu>
2014-10-28 15:18:00 -0700

    add reorder queues and lookup

---
+++ lib/hw/contrib/pcores/nf10_bram_reorder_output_queues_v1_00_a/hdl/verilog/nf10_bram_reorder_output_queues.v
@@ -0,0 +1,379 @@
+/*******************************************************************************
+ *
+ *  NetFPGA-10G http://www.netfpga.org
+ *
+ *  File:
+ *        nf10_bram_output_queues.v
+ *
+ *  Library:
+ *        hw/std/pcores/nf10_bram_output_queues_v1_00_a
+ *
+ *  Module:
+ *        nf10_bram_output_queues
+ *
+ *  Author:
+ *        Yilong Geng, James Hongyi Zeng
+ *
+ *  Description:
+ *        BRAM Output queues
+ *        Outputs have a parameterizable width
+ *
+ *  Copyright notice:
+ *        Copyright (C) 2010, 2011 The Board of Trustees of The Leland Stanford
+ *                                 Junior University
+ *
+ *  Licence:
+ *        This file is part of the NetFPGA 10G development base package.
+ *
+ *        This file is free code: you can redistribute it and/or modify it under
+ *        the terms of the GNU Lesser General Public License version 2.1 as
+ *        published by the Free Software Foundation.
+ *
+ *        This package is distributed in the hope that it will be useful, but
+ *        WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *        Lesser General Public License for more details.
+ *
+ *        You should have received a copy of the GNU Lesser General Public
+ *        License along with the NetFPGA source package.  If not, see
+ *        http://www.gnu.org/licenses/.
+ *
+ */
+
+module nf10_bram_reorder_output_queues
+#(
+    // Master AXI Stream Data Width
+    parameter C_M_AXIS_DATA_WIDTH=256,
+    parameter C_S_AXIS_DATA_WIDTH=256,
+    parameter C_M_AXIS_TUSER_WIDTH=128,
+    parameter C_S_AXIS_TUSER_WIDTH=128,
+    parameter NUM_QUEUES=5
+)
+(
+    // Part 1: System side signals
+    // Global Ports
+    input axi_aclk,
+    input axi_resetn,
+
+    // Slave Stream Ports (interface to data path)
+    input [C_S_AXIS_DATA_WIDTH - 1:0] s_axis_tdata,
+    input [((C_S_AXIS_DATA_WIDTH / 8)) - 1:0] s_axis_tstrb,
+    input [C_S_AXIS_TUSER_WIDTH-1:0] s_axis_tuser,
+    input s_axis_tvalid,
+    output s_axis_tready,
+    input s_axis_tlast,
+
+    // Master Stream Ports (interface to TX queues)
+    output [C_M_AXIS_DATA_WIDTH - 1:0] m_axis_tdata_0,
+    output [((C_M_AXIS_DATA_WIDTH / 8)) - 1:0] m_axis_tstrb_0,
+    output [C_M_AXIS_TUSER_WIDTH-1:0] m_axis_tuser_0,
+    output  m_axis_tvalid_0,
+    input m_axis_tready_0,
+    output  m_axis_tlast_0,
+
+    output [C_M_AXIS_DATA_WIDTH - 1:0] m_axis_tdata_1,
+    output [((C_M_AXIS_DATA_WIDTH / 8)) - 1:0] m_axis_tstrb_1,
+    output [C_M_AXIS_TUSER_WIDTH-1:0] m_axis_tuser_1,
+    output  m_axis_tvalid_1,
+    input m_axis_tready_1,
+    output  m_axis_tlast_1,
+
+    output [C_M_AXIS_DATA_WIDTH - 1:0] m_axis_tdata_2,
+    output [((C_M_AXIS_DATA_WIDTH / 8)) - 1:0] m_axis_tstrb_2,
+    output [C_M_AXIS_TUSER_WIDTH-1:0] m_axis_tuser_2,
+    output  m_axis_tvalid_2,
+    input m_axis_tready_2,
+    output  m_axis_tlast_2,
+
+    output [C_M_AXIS_DATA_WIDTH - 1:0] m_axis_tdata_3,
+    output [((C_M_AXIS_DATA_WIDTH / 8)) - 1:0] m_axis_tstrb_3,
+    output [C_M_AXIS_TUSER_WIDTH-1:0] m_axis_tuser_3,
+    output  m_axis_tvalid_3,
+    input m_axis_tready_3,
+    output  m_axis_tlast_3,
+
+    output [C_M_AXIS_DATA_WIDTH - 1:0] m_axis_tdata_4,
+    output [((C_M_AXIS_DATA_WIDTH / 8)) - 1:0] m_axis_tstrb_4,
+    output [C_M_AXIS_TUSER_WIDTH-1:0] m_axis_tuser_4,
+    output  m_axis_tvalid_4,
+    input m_axis_tready_4,
+    output  m_axis_tlast_4
+);
+
+   function integer log2;
+      input integer number;
+      begin
+         log2=0;
+         while(2**log2<number) begin
+            log2=log2+1;
+         end
+      end
+   endfunction // log2
+
+   // ------------ Internal Params --------
+
+   localparam NUM_QUEUES_WIDTH = log2(NUM_QUEUES);
+
+   localparam BUFFER_SIZE         = 32768; // Buffer size 32768B, around 20 packets
+   localparam BUFFER_SIZE_WIDTH   = log2(BUFFER_SIZE/(C_M_AXIS_DATA_WIDTH/8));
+
+   localparam MAX_PACKET_SIZE = 1600;
+   localparam BUFFER_THRESHOLD = (BUFFER_SIZE-MAX_PACKET_SIZE)/(C_M_AXIS_DATA_WIDTH/8);
+
+   localparam NUM_STATES = 3;
+   localparam IDLE = 0;
+   localparam WR_PKT = 1;
+   localparam DROP = 2;
+
+   localparam NUM_METADATA_STATES = 2;
+   localparam WAIT_HEADER = 0;
+   localparam WAIT_EOP = 1;
+
+   // ------------- Regs/ wires -----------
+
+   wire                             fifo_in_nearly_full;
+   wire                             fifo_in_empty;
+   reg                              fifo_in_rd_en;
+   wire [C_M_AXIS_TUSER_WIDTH-1:0]  fifo_in_tuser;
+   wire [C_M_AXIS_DATA_WIDTH-1:0]   fifo_in_tdata;
+   wire [C_M_AXIS_DATA_WIDTH/8-1:0] fifo_in_tstrb;
+   wire  	                    fifo_in_tlast;
+   wire                             fifo_in_tvalid;
+
+   reg [NUM_QUEUES-1:0]                nearly_full;
+   wire [NUM_QUEUES-1:0]               nearly_full_fifo;
+   wire [NUM_QUEUES-1:0]               empty;
+
+   reg [NUM_QUEUES-1:0]                metadata_nearly_full;
+   wire [NUM_QUEUES-1:0]               metadata_nearly_full_fifo;
+   wire [NUM_QUEUES-1:0]               metadata_empty;
+
+   wire [C_M_AXIS_TUSER_WIDTH-1:0]             fifo_out_tuser[NUM_QUEUES-1:0];
+   wire [C_M_AXIS_DATA_WIDTH-1:0]        fifo_out_tdata[NUM_QUEUES-1:0];
+   wire [((C_M_AXIS_DATA_WIDTH/8))-1:0]  fifo_out_tstrb[NUM_QUEUES-1:0];
+   wire [NUM_QUEUES-1:0] 	           fifo_out_tlast;
+
+   wire [NUM_QUEUES-1:0]               rd_en;
+   reg [NUM_QUEUES-1:0]                wr_en;
+
+   reg [NUM_QUEUES-1:0]                metadata_rd_en;
+   reg [NUM_QUEUES-1:0]                metadata_wr_en;
+
+   reg [NUM_QUEUES-1:0]          cur_queue;
+   reg [NUM_QUEUES-1:0]          cur_queue_next;
+   wire [NUM_QUEUES-1:0]         oq;
+
+   reg [NUM_STATES-1:0]                state;
+   reg [NUM_STATES-1:0]                state_next;
+
+   reg [NUM_METADATA_STATES-1:0]       metadata_state[NUM_QUEUES-1:0];
+   reg [NUM_METADATA_STATES-1:0]       metadata_state_next[NUM_QUEUES-1:0];
+
+   reg								   first_word, first_word_next;
+
+   // ------------ Modules -------------
+
+   fallthrough_small_fifo
+   #( .WIDTH(C_M_AXIS_DATA_WIDTH+C_M_AXIS_TUSER_WIDTH+C_M_AXIS_DATA_WIDTH/8+1),
+      .MAX_DEPTH_BITS(2)
+    )
+    input_fifo
+    ( // Outputs
+      .dout                         ({fifo_in_tlast, fifo_in_tuser, fifo_in_tstrb, fifo_in_tdata}),
+      .full                         (),
+      .nearly_full                  (fifo_in_nearly_full),
+      .prog_full                    (),
+      .empty                        (fifo_in_empty),
+      // Inputs
+      .din                          ({s_axis_tlast, s_axis_tuser, s_axis_tstrb, s_axis_tdata}),
+      .wr_en                        (s_axis_tvalid & s_axis_tready),
+      .rd_en                        (fifo_in_rd_en),
+      .reset                        (~axi_resetn),
+      .clk                          (axi_aclk));
+
+   assign s_axis_tready = !fifo_in_nearly_full;
+
+   generate
+   genvar i;
+   for(i=0; i<NUM_QUEUES; i=i+1) begin: output_queues
+      fallthrough_small_fifo
+        #( .WIDTH(C_M_AXIS_DATA_WIDTH+C_M_AXIS_DATA_WIDTH/8+1),
+           .MAX_DEPTH_BITS(BUFFER_SIZE_WIDTH),
+           .PROG_FULL_THRESHOLD(BUFFER_THRESHOLD))
+      output_fifo
+        (// Outputs
+         .dout                           ({fifo_out_tlast[i], fifo_out_tstrb[i], fifo_out_tdata[i]}),
+         .full                           (),
+         .nearly_full                    (),
+	 	 .prog_full                      (nearly_full_fifo[i]),
+         .empty                          (empty[i]),
+         // Inputs
+         .din                            ({fifo_in_tlast, fifo_in_tstrb, fifo_in_tdata}),
+         .wr_en                          (wr_en[i]),
+         .rd_en                          (rd_en[i]),
+         .reset                          (~axi_resetn),
+         .clk                            (axi_aclk));
+
+      fallthrough_small_fifo
+        #( .WIDTH(C_M_AXIS_TUSER_WIDTH),
+           .MAX_DEPTH_BITS(5))
+      metadata_fifo
+        (// Outputs
+         .dout                           (fifo_out_tuser[i]),
+         .full                           (),
+         .nearly_full                    (metadata_nearly_full_fifo[i]),
+	 	 .prog_full                      (),
+         .empty                          (metadata_empty[i]),
+         // Inputs
+         .din                            (fifo_in_tuser),
+         .wr_en                          (metadata_wr_en[i]),
+         .rd_en                          (metadata_rd_en[i]),
+         .reset                          (~axi_resetn),
+         .clk                            (axi_aclk));
+
+   always @(metadata_state[i], rd_en[i], fifo_out_tlast[i]) begin
+        metadata_rd_en[i] = 1'b0;
+        metadata_state_next[i] = metadata_state[i];
+      	case(metadata_state[i])
+      		WAIT_HEADER: begin
+      			if(rd_en[i]) begin
+      				metadata_state_next[i] = WAIT_EOP;
+      				metadata_rd_en[i] = 1'b1;
+      			end
+      		end
+      		WAIT_EOP: begin
+      			if(rd_en[i] & fifo_out_tlast[i]) begin
+      				metadata_state_next[i] = WAIT_HEADER;
+      			end
+      		end
+        endcase
+      end
+
+      always @(posedge axi_aclk) begin
+      	if(~axi_resetn) begin
+         	metadata_state[i] <= WAIT_HEADER;
+      	end
+      	else begin
+         	metadata_state[i] <= metadata_state_next[i];
+      	end
+      end
+   end
+   endgenerate
+
+   always @(*) begin
+      state_next     = state;
+      cur_queue_next = cur_queue;
+      wr_en          = 0;
+      metadata_wr_en = 0;
+      fifo_in_rd_en  = 0;
+      first_word_next = first_word;
+
+      case(state)
+
+        /* cycle between input queues until one is not empty */
+        IDLE: begin
+           if(!fifo_in_empty) begin
+              if(~|((nearly_full | metadata_nearly_full) & cur_queue)) begin // All interesting oqs are NOT _nearly_ full (able to fit in the maximum pacekt).
+                  state_next = WR_PKT;
+                  first_word_next = 1'b1;
+              end
+              else begin
+              	  state_next = DROP;
+              end
+           end
+        end
+
+        /* wait until eop */
+        WR_PKT: begin
+            if(!fifo_in_empty) begin
+           	    fifo_in_rd_en = 1;
+           		  first_word_next = 1'b0;
+				        wr_en = cur_queue;
+				        if(first_word) begin
+					          metadata_wr_en = cur_queue;
+				        end
+				        if(fifo_in_tlast) begin
+                    if(|(cur_queue & 5'b10000)) begin
+                        cur_queue_next = 5'b00001;
+                    end
+                    else begin
+                        cur_queue_next = (cur_queue << 1);
+                    end
+					          state_next = IDLE;
+				        end
+            end
+        end // case: WR_PKT
+
+        DROP: begin
+            if(!fifo_in_empty) begin
+                fifo_in_rd_en = 1;
+		            if (fifo_in_tlast) begin
+                    if(|(cur_queue & 5'b10000)) begin
+                        cur_queue_next = 5'b00001;
+                    end
+                    else begin
+                        cur_queue_next = (cur_queue << 1);
+                    end
+           	        state_next = IDLE;
+		            end
+            end
+        end
+
+      endcase // case(state)
+   end // always @ (*)
+
+
+
+   always @(posedge axi_aclk) begin
+      if(~axi_resetn) begin
+         state <= IDLE;
+         cur_queue <= 1;
+         first_word <= 0;
+      end
+      else begin
+         state <= state_next;
+         cur_queue <= cur_queue_next;
+         first_word <= first_word_next;
+      end
+
+      nearly_full <= nearly_full_fifo;
+      metadata_nearly_full <= metadata_nearly_full_fifo;
+   end
+
+
+   assign m_axis_tdata_0	 = fifo_out_tdata[0];
+   assign m_axis_tstrb_0	 = fifo_out_tstrb[0];
+   assign m_axis_tuser_0	 = fifo_out_tuser[0];
+   assign m_axis_tlast_0	 = fifo_out_tlast[0];
+   assign m_axis_tvalid_0	 = ~empty[0];
+   assign rd_en[0]			 = m_axis_tready_0 & ~empty[0];
+
+   assign m_axis_tdata_1	 = fifo_out_tdata[1];
+   assign m_axis_tstrb_1	 = fifo_out_tstrb[1];
+   assign m_axis_tuser_1	 = fifo_out_tuser[1];
+   assign m_axis_tlast_1	 = fifo_out_tlast[1];
+   assign m_axis_tvalid_1	 = ~empty[1];
+   assign rd_en[1]			 = m_axis_tready_1 & ~empty[1];
+
+   assign m_axis_tdata_2	 = fifo_out_tdata[2];
+   assign m_axis_tstrb_2	 = fifo_out_tstrb[2];
+   assign m_axis_tuser_2	 = fifo_out_tuser[2];
+   assign m_axis_tlast_2	 = fifo_out_tlast[2];
+   assign m_axis_tvalid_2	 = ~empty[2];
+   assign rd_en[2]			 = m_axis_tready_2 & ~empty[2];
+
+   assign m_axis_tdata_3	 = fifo_out_tdata[3];
+   assign m_axis_tstrb_3	 = fifo_out_tstrb[3];
+   assign m_axis_tuser_3	 = fifo_out_tuser[3];
+   assign m_axis_tlast_3	 = fifo_out_tlast[3];
+   assign m_axis_tvalid_3	 = ~empty[3];
+   assign rd_en[3]			 = m_axis_tready_3 & ~empty[3];
+
+  assign m_axis_tdata_4  = fifo_out_tdata[4];
+   assign m_axis_tstrb_4   = fifo_out_tstrb[4];
+   assign m_axis_tuser_4   = fifo_out_tuser[4];
+   assign m_axis_tlast_4   = fifo_out_tlast[4];
+   assign m_axis_tvalid_4  = ~empty[4];
+   assign rd_en[4]       = m_axis_tready_4 & ~empty[4];
+
+endmodule
---
+++ lib/hw/contrib/pcores/nf10_reorder_output_port_lookup_v1_00_a/hdl/verilog/nf10_reorder_output_port_lookup.v
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ *
+ *  NetFPGA-10G http://www.netfpga.org
+ *
+ *  File:
+ *        nf10_nic_output_port_lookup.v
+ *
+ *  Library:
+ *        hw/std/pcores/nf10_nic_output_port_lookup_v1_00_a
+ *
+ *  Module:
+ *        nf10_nic_output_port_lookup
+ *
+ *  Author:
+ *        Yilong Geng, Adam Covington
+ *
+ *  Description:
+ *        Hardwire the hardware interfaces to CPU and vice versa
+ *
+ *  Copyright notice:
+ *        Copyright (C) 2010, 2011 The Board of Trustees of The Leland Stanford
+ *                                 Junior University
+ *
+ *  Licence:
+ *        This file is part of the NetFPGA 10G development base package.
+ *
+ *        This file is free code: you can redistribute it and/or modify it under
+ *        the terms of the GNU Lesser General Public License version 2.1 as
+ *        published by the Free Software Foundation.
+ *
+ *        This package is distributed in the hope that it will be useful, but
+ *        WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *        Lesser General Public License for more details.
+ *
+ *        You should have received a copy of the GNU Lesser General Public
+ *        License along with the NetFPGA source package.  If not, see
+ *        http://www.gnu.org/licenses/.
+ *
+ */
+
+module nf10_reorder_output_port_lookup
+#(
+    //Master AXI Stream Data Width
+    parameter C_M_AXIS_DATA_WIDTH=256,
+    parameter C_S_AXIS_DATA_WIDTH=256,
+    parameter C_M_AXIS_TUSER_WIDTH=128,
+    parameter C_S_AXIS_TUSER_WIDTH=128,
+    parameter SRC_PORT_POS=16,
+    parameter DST_PORT_POS=24
+)
+(
+    // Global Ports
+    input axi_aclk,
+    input axi_resetn,
+
+    // Master Stream Ports (interface to data path)
+    output [C_M_AXIS_DATA_WIDTH - 1:0] m_axis_tdata,
+    output [((C_M_AXIS_DATA_WIDTH / 8)) - 1:0] m_axis_tstrb,
+    output reg [C_M_AXIS_TUSER_WIDTH-1:0] m_axis_tuser,
+    output m_axis_tvalid,
+    input  m_axis_tready,
+    output m_axis_tlast,
+
+    // Slave Stream Ports (interface to RX queues)
+    input [C_S_AXIS_DATA_WIDTH - 1:0] s_axis_tdata,
+    input [((C_S_AXIS_DATA_WIDTH / 8)) - 1:0] s_axis_tstrb,
+    input [C_S_AXIS_TUSER_WIDTH-1:0] s_axis_tuser,
+    input  s_axis_tvalid,
+    output s_axis_tready,
+    input  s_axis_tlast
+);
+
+   function integer log2;
+      input integer number;
+      begin
+         log2=0;
+         while(2**log2<number) begin
+            log2=log2+1;
+         end
+      end
+   endfunction // log2
+
+   // ------------ Internal Params --------
+   localparam MODULE_HEADER = 0;
+   localparam IN_PACKET     = 1;
+
+   //------------- Wires ------------------
+   wire  [C_M_AXIS_TUSER_WIDTH-1:0] tuser_fifo;
+   reg 			  state, state_next;
+
+   // ------------ Modules ----------------
+
+   fallthrough_small_fifo
+        #( .WIDTH(C_M_AXIS_DATA_WIDTH+C_M_AXIS_TUSER_WIDTH+C_M_AXIS_DATA_WIDTH/8+1),
+           .MAX_DEPTH_BITS(2))
+      input_fifo
+        (// Outputs
+         .dout                           ({m_axis_tlast, tuser_fifo, m_axis_tstrb, m_axis_tdata}),
+         .full                           (),
+         .nearly_full                    (in_fifo_nearly_full),
+         .prog_full                      (),
+         .empty                          (in_fifo_empty),
+         // Inputs
+         .din                            ({s_axis_tlast, s_axis_tuser, s_axis_tstrb, s_axis_tdata}),
+         .wr_en                          (s_axis_tvalid & ~in_fifo_nearly_full),
+         .rd_en                          (in_fifo_rd_en),
+         .reset                          (~axi_resetn),
+         .clk                            (axi_aclk));
+
+   // ------------- Logic ----------------
+
+   assign s_axis_tready = !in_fifo_nearly_full;
+
+   // packet is from the cpu if it is on an odd numbered port
+   assign pkt_is_from_cpu = m_axis_tuser[SRC_PORT_POS+1] ||
+			    m_axis_tuser[SRC_PORT_POS+3] ||
+			    m_axis_tuser[SRC_PORT_POS+5] ||
+			    m_axis_tuser[SRC_PORT_POS+7];
+
+   // modify the dst port in tuser
+   always @(*) begin
+      m_axis_tuser = tuser_fifo;
+      state_next      = state;
+
+      case(state)
+	MODULE_HEADER: begin
+	   if (m_axis_tvalid) begin
+	      if((~|m_axis_tuser[SRC_PORT_POS+:8]) | pkt_is_from_cpu) begin
+	      	m_axis_tuser[DST_PORT_POS+7:DST_PORT_POS] = 8'b1;
+	      end // Default: Send to MAC 0
+	      else begin
+		        case(m_axis_tuser[SRC_PORT_POS+:8])
+                8'b00000001:
+                    m_axis_tuser[DST_PORT_POS+7:DST_PORT_POS] = 8'b00000100;
+                end
+                8'b00000100:
+                    m_axis_tuser[DST_PORT_POS+7:DST_PORT_POS] = 8'b00000001;
+                end
+                8'b00010000:
+                    m_axis_tuser[DST_PORT_POS+7:DST_PORT_POS] = 8'b01000000;
+                end
+                8'b01000000:
+                    m_axis_tuser[DST_PORT_POS+7:DST_PORT_POS] = 8'b00010000;
+                end
+            endcase
+	      end
+	      if(m_axis_tready) begin
+			    state_next = IN_PACKET;
+			end
+	   end
+	end // case: MODULE_HEADER
+
+	IN_PACKET: begin
+	   if(m_axis_tlast & m_axis_tvalid & m_axis_tready) begin
+	      state_next = MODULE_HEADER;
+	   end
+	end
+      endcase // case (state)
+   end // always @ (*)
+
+   always @(posedge axi_aclk) begin
+      if(~axi_resetn) begin
+	 state <= MODULE_HEADER;
+      end
+      else begin
+	 state <= state_next;
+      end
+   end
+
+   // Handle output
+   assign in_fifo_rd_en = m_axis_tready && !in_fifo_empty;
+   assign m_axis_tvalid = !in_fifo_empty;
+
+endmodule // output_port_lookup
